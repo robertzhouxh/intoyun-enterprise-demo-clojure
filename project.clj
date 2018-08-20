2eeca567e272ecc2b15ab5032984ba6eba1b6793
robertzhouxh <robertzhouxh@gmail.com>
2018-08-20 23:03:16 +0800

    add deps for ws

--- project.clj
+++ project.clj
@@ -1,17 +1,19 @@
 (defproject web "1.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/java.jdbc "0.7.8"]
+                 ;[org.clojure/java.jdbc "0.7.8"]
                  [com.taoensso/timbre "4.10.0"]           ;; log lib
                  [ring "1.7.0-RC1"]                       ;; http lib
                  [ring/ring-json "0.4.0"]                 ;; middleware
                  [compojure "1.6.1"]                      ;; router
                  [spootnik/kinsky "0.1.22"]               ;; kafka lib
-                 [mysql/mysql-connector-java "8.0.11"]    ;; mysql driver
+                 ;[mysql/mysql-connector-java "8.0.12"]   ;; mysql driver 
+                 [mysql/mysql-connector-java "5.1.38"]    ;; mysql driver: new driver class is `com.mysql.cj.jdbc.Driver since 6.*.*
                  [korma "0.4.3"]                          ;; mysql wrap
                  [cheshire "5.8.0"]                       ;; json
                  [base64-clj "0.1.1"]                     ;; base64
                  [robertzhouxh/buddy-core "1.5.1"]        ;; crypto(aes)
                  [funcool/cuerdas "2.0.5"]                ;; string <http://funcool.github.io/cuerdas/latest/>
+                 [aleph "0.4.6"]                          ;; websocket/tcp/udp
                  ;; ;; fix bug for SLF4J >>>>>> Failed to load class ...
                  [org.slf4j/slf4j-log4j12 "1.7.25"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
